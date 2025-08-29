f9d0f9ce6223784d1b506a61dc697764b456ee50
lrs5cz <lrs5cz@gmail.com>
2025-08-29 08:24:58 -0600

    Update merge_sort.c
    Algoritmo aplicado

--- merge_sort.c
+++ merge_sort.c
@@ -34,6 +34,7 @@ void merge(int arr[], int left, int mid, int right);
 
 int main() {
     int n;
+    printf("Ingrese el número de datos y arreglos a ordenar (n ^ 2): ");
     scanf("%d", &n);  // Leer el número de arreglos
 
     int total = n * n;  
@@ -41,12 +42,15 @@ int main() {
 
     // Leer n arreglos de tamaño n
     for (int i = 0; i < total; i++) {
+        printf("Ingrese el número %d de tu arreglo %d: ", i + 1, i + 1);
         scanf("%d", &arr[i]);
     }
 
     // Ordenar usando merge sort
     mergeSort(arr, 0, total - 1);
-
+    // Salto de línea
+    printf("\n");
+    
     // Imprimir el arreglo ordenado
     for (int i = 0; i < total; i++) {
         printf("%d ", arr[i]);
@@ -59,10 +63,56 @@ int main() {
 
 // ---- Implementa aquí tu función mergeSort ----
 void mergeSort(int arr[], int left, int right) {
-    // TODO
-}
+    if (left < right) {
+        
+        int mid = left + (right - left) / 2;
+        
+        mergeSort(arr,left, mid);
+        
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+        }
+    }
 
 // ---- Implementa aquí tu función merge ----
 void merge(int arr[], int left, int mid, int right) {
-    // TODO
+    int i, j, k;
+    int s1 = mid - left + 1;
+    int s2 = right - mid;
+    
+    int left_arr[s1], right_arr[s2];
+    
+    for(i = 0; i < s1; i++) {
+        left_arr[i] = arr[left + i];
+    }
+    
+    for (j = 0; j < s2; j++) {
+        right_arr[j] = arr[mid + 1 + j];
+    }
+    
+    i = 0, j = 0;
+    k = left;
+    
+    while(i < s1 && j < s2) {
+        if (left_arr[i] <= right_arr[j]) {
+            arr[k] = left_arr[i];
+            i++;
+        } else {
+            arr[k] = right_arr[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < s1) {
+        arr[k] = left_arr[i];
+        i++;
+        k++;
+    }
+    
+    while (j < s2) {
+        arr[k] = right_arr[j];
+        j++;
+        k++;
+    }
 }
